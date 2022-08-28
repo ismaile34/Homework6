7a82cb28b857605a698769b05961acac9568f6e4
ismail <511is@mail.ru>
2022-08-28 18:41:11 +0300

    Homrwork 6

---
+++ exercise1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    int result = Convert.ToInt32(Console.ReadLine()); // Преобразует строку в целое число
+    return result; // Возвращает результат функции
+}
+
+void NumberCheck(int number)
+{
+    if (number > 0)
+    {
+        int [] Array = InputNumbers(number);
+        ArrayOut(Array);
+        System.Console.WriteLine($"Количество чисел больще нуля =  {PosElement(Array)}");
+    }
+    else
+    {
+        System.Console.WriteLine("STOP");
+    }
+}
+
+int [] InputNumbers(int number)
+{
+    int[] Array = new int [number];
+    int i = 0;
+    while (i < number)
+    {
+        System.Console.Write("Введите число > ");
+        Array[i] = int.Parse(Console.ReadLine());
+        i++;
+    }
+    return Array;
+}
+
+int PosElement(int[] Array)
+{
+    int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void ArrayOut(int[] Array)
+{
+    for (int j = 0; j < Array.Length; j++)
+    {
+        System.Console.WriteLine($"{Array[j]}");
+    }
+}
+int number = Prompt("Введите количество чисел > ");
+NumberCheck(number);
---
+++ exercise2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void CheckLines(double k1, double b1, double k2, double b2)
+{
+    if (b1 == b2 && k1 == k2)
+    {
+        System.Console.WriteLine("Прымые равны");
+    }
+    else if (k1 == k2)
+    {
+        System.Console.WriteLine("Прымые параллельны");
+    }
+    else
+    {
+        (var x, var y) = Points(k1, k2, b1, b2);
+        System.Console.WriteLine($"Координаты точки пересечения = ({x:f2}; {y:f2})");
+    }
+}
+
+(double, double) Points(double k1, double k2, double b1, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return (x, y);
+}
+
+int k1 = Prompt("Введите значение k1 >");
+int b1 = Prompt("Введите значение b1 >");
+int k2 = Prompt("Введите значение k2 >");
+int b2 = Prompt("Введите значение b2 >");
+
+CheckLines(k1, b1, k2, b2);
